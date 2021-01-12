8fbfeb30bb2f330c93369d504ba22c2ee06f8ed5
keeeva <70092439+keeeva@users.noreply.github.com>
2021-01-12 22:55:50 +0100

    Update cachematrix.R
    completed two functions makeCacheMatrix() and cacheSolve()

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,46 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This function will instatiate a CacheMatrix with getters and setters,
+## furthermore the result (i.e. the inverse) will be cached so that it does
+## not have to be re-computed once it has already been computed.
 
-## Write a short comment describing this function
+## here we are just initializing, setting setters and getters
 
-makeCacheMatrix <- function(x = matrix()) {
 
+makeCacheMatrix <- function(matrx = matrix()) {
+                # here the inverse i is instantiated
+                i <- NULL
+                        
+                # setter for matrx
+                set <- function(new_matrx) {
+                        matrx <<- new_matrx
+                        i <<- NULL
+                }
+                # getter for mat
+                get <- function() matrx
+                
+                # setter for i
+                setinverse <- function(new_i) i <<- new_i
+                
+                # getter for i
+                getinverse <- function() i
+                
+                # list that stores the values so that they can be accessed
+                list(set = set, get = get,
+                        setinverse = setinverse,
+                        getinverse = getinverse)   
 }
 
+## if computation of inverse stored return it else compute it
 
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(c_mtrx, ...) {
+        # returns cached computation of inverse       
+                  i <- c_mtrx$getinverse()
+                  if (!is.null(i)) {
+                          message("getting cached data")
+                          return(i)
+                  }
+                  data <- c_mtrx$get()
+                  i <- solve(data, ...)
+                  c_mtrx$setinverse(i)
+                  i
 }
+
